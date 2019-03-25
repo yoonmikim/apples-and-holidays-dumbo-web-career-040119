8b815d74afd6a1e8e094be89153845cbfcd773cf
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-25 02:48:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_supplies[:winter].map do |k, v|
     v
-  end
+  end.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
