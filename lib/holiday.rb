42b3af28fbba3e3f9443099b948f9c3c70a337cb
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-25 03:51:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts "#{holiday.to_s.split('_').map {|w| w.capitalize}.join(" ")}: #{supplies.join(", ").capitalize}"
+      puts "#{holiday.to_s.split('_').map {|w| w.capitalize}.join(" ")}: #{supplies.join(", ")}"
     end
   end
 end
