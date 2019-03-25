8c9be4c9331e6fa22878f0104a63c1cd9b2d138d
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-25 03:48:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash
+  holiday_hash.map do |season, holiday|
+    holiday.
+  end
 end
 
 
